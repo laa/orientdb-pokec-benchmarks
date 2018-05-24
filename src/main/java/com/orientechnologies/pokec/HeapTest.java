6bb7b0eb67ace6533dadd4db7b515fa02b77e46a
laa <lomakin.andrey@gmail.com>
2018-05-24 15:51:52 +0200

    Heap test

---
+++ src/main/java/com/orientechnologies/pokec/HeapTest.java
@@ -0,0 +1,8 @@
+package com.orientechnologies.pokec;
+
+public class HeapTest {
+  public static void main(String[] args) throws Exception {
+    byte[] oneMArray = new byte[1024 * 124];
+    System.in.read();
+  }
+}
