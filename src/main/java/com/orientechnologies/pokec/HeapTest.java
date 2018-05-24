b001ac0a074d0ca67dfd7516cd06145de99adc28
laa <lomakin.andrey@gmail.com>
2018-05-24 15:55:59 +0200

    Heap test wait for 10 min

--- src/main/java/com/orientechnologies/pokec/HeapTest.java
+++ src/main/java/com/orientechnologies/pokec/HeapTest.java
@@ -2,7 +2,7 @@ package com.orientechnologies.pokec;
 
 public class HeapTest {
   public static void main(String[] args) throws Exception {
-    byte[] oneMArray = new byte[1024 * 124];
-    System.in.read();
+    byte[] oneMArray = new byte[1024 * 1024];
+   Thread.sleep(10 * 10 * 1000);
   }
 }
